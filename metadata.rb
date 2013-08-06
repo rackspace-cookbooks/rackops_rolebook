fd6ac022ad5cf938624c15dfb8180b556e24c744
Ryan Richard <ryanrichard07@gmail.com>
2013-08-06 10:36:52 -0500

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.richard@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures rackops-rolebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends "rackspace-user"
 depends "motd"
