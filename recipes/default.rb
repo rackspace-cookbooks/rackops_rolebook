31ad25ae8fc2ca1ea8486bfedfb7a34c3a9feff9
Thomas Cate <thomas.cate@rackspace.com>
2014-02-14 09:29:11 -0600

    check if server is configured, since client may not be set

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,9 +17,9 @@ if node['rackops-rolebook']['include_acl'] == true
   critical_recipes.push('rackops-rolebook::acl')
 end
 
-# Only include chef-client in client mode.
+# Only include chef-client if a chef server url is specified.
 # This should allow for testing via solo
-if Chef::Config[:client] == true
+if Chef::Config[:chef_server_url]
   critical_recipes.push('chef-client')
   critical_recipes.push('chef-client::delete_validation')
   critical_recipes.push('chef-client::config')
