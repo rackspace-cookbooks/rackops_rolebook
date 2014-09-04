f95e3e47d27f1601420b3e6c967c1752d2486485
Matt Barlow <matt.barlow@rackspace.com>
2014-09-04 17:21:58 -0500

    Consolidate Tests and Update Rakefile
    Bundle exec rake default was not completing successfully. I removed
    Debian from .kitchen.yml, since it was causing TK to fail. We can add it
    back in later if necessary. I deleted the spec directory and created one
    called test, and subdirectories for serverspec and chefspec. Finally, I
    created chefspec tests for public_info.rb since I'm working on issue

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures rackops_rolebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '3.1.1'
+version          '3.1.2'
 
 depends 'apt', '>= 2.4'
 depends 'git'
---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,5 @@
+# Encoding: utf-8
+require_relative 'spec_helper'
+describe 'default' do
+  it { pending 'write some tests' }
+end
