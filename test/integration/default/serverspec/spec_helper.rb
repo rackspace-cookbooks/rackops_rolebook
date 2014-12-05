0dd713558b4fbb70d33f14124f349a4a3b9445ae
Martin Smith <martin@mbs3.org>
2014-12-05 08:59:42 -0600

    Fix typo in exec vs. :exec, add a test

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,10 +1,10 @@
 # Encoding: utf-8
 require 'serverspec'
 
-set :backend, exec
+set :backend, :exec
 
 RSpec.configure do |c|
   c.before :all do
-    c.path = '/sbin:/usr/bin'
+    c.path = '/sbin:/bin:/usr/bin:/usr/sbin'
   end
 end
